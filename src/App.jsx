9e82ec4baff1be0a0ea6ce88eec1966e3d007f07
dimakotb <dimasobhy12@gmail.com>
2025-03-31 23:26:16 +0200

    Pages, Components, react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import FoodLogo from './assets/FoodDeliveryLogo.svg'
-import viteLogo from '/vite.svg'
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header"; 
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Account from "./pages/Account";
+import Cart from "./pages/Cart";
+import Checkout from "./pages/Checkout";
+import Login from "./pages/Login";
+import Menus from "./pages/Menus";
+import Orders from "./pages/Orders";
+import Restaurants from "./pages/Restaurants";
+import UserOrders from "./pages/UserOrders";
+import ListItems from "./pages/ListItems";
+import AddItems from "./pages/AddItems";
+import FAQ from "./pages/FAQ";
+
+export default function App () {
+  return (
+    <main>
+    <Header> 
+      <Routes>
+        <Route path="/" element = {<Home />}/>
+        <Route path="/about" element = {<About />}/>
+        <Route path="/account" element = {<Account />}/>
+        <Route path="/cart" element = {<Cart />}/>
+        <Route path="/checkout" element = {<Checkout />}/>
+        <Route path="/login" element = {<Login />}/>
+        <Route path="/menus" element = {<Menus />}/>
+        <Route path="/orders" element = {<Orders />}/>
+        <Route path="/restaurants" element = {<Restaurants />}/>
+        <Route path="/user-orders" element = {<UserOrders />}/>
+        <Route path="/list-items" element = {<ListItems/>}/>
+        <Route path="/add-items" element = {<AddItems/>}/>
+        <Route path="/faq" element = {<FAQ/>}/>
+      </Routes>
+    </Header>
+    </main>
+  )
+}
+
+
+
+/* import FoodLogo from './assets/FoodDeliveryLogo.svg'
 import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div class="logo-container">
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
         <a target='_blank'>
           <img src={FoodLogo} class="food" alt="Food logo" />
         </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
       </div>
-      <h1 class="text-3xl font-bold underline hover:text-sky-700 ">Vite + React</h1>
+      <h1 class="text-3xl font-bold hover:text-sky-700 ">Crave Cart</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
 
-export default App
+export default App */ 
---
+++ src/pages/ListItems.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ListItems = () => {
+  return (
+    <div>ListItems</div>
+  )
+}
+
+export default ListItems
